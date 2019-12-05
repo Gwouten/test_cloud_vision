8628dde6269ad322e78df238bc2b12ed2c159c21
Gwouten <wouter.deloof@gmail.com>
2019-12-05 22:24:15 +0100

    Initial commit

---
+++ src/main/java/deloof/wouter/test_cloud_vision/TestCloudVisionApplication.java
@@ -0,0 +1,13 @@
+package deloof.wouter.test_cloud_vision;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestCloudVisionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestCloudVisionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/deloof/wouter/test_cloud_vision/TestCloudVisionApplicationTests.java
@@ -0,0 +1,13 @@
+package deloof.wouter.test_cloud_vision;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestCloudVisionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
