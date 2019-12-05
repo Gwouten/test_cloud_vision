bb7a8afc4fbc4528838cf7174df86a0677e748d0
Gwouten <wouter.deloof@gmail.com>
2019-12-05 22:33:45 +0100

    Created Class and DAO for cards.

---
+++ src/main/java/deloof/wouter/test_cloud_vision/model/CardDAO.java
@@ -0,0 +1,9 @@
+package deloof.wouter.test_cloud_vision.model;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CardDAO extends CrudRepository<Card, Integer> {
+
+
+
+}
